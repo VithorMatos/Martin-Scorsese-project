762d4fa07d9b598d65a377bdf18415404f8c1a33
Vithor <vithor_matos@hotmail.com>
2024-09-12 08:16:50 -0300

    shows feito

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao){
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active')
+        })
+    }
+})  
+
+function escondeTodasAbas() {
+    const tabsContainer =  document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active')
+    }
+}
